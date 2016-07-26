3073a03e7d2d801226c525e574f9bba295e12ddd
daniel <daniel@yunify.com>
2016-07-26 15:57:54 +0800

    Change version number to '1.0.3'
    Signed-off-by: daniel <daniel@yunify.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'qsctl',
-    version = '1.0.2',
+    version = '1.0.3',
     description = 'Advanced command line tool for QingStor.',
     long_description = open('README.rst', 'rb').read().decode('utf-8'),
     keywords = 'qingcloud qingstor qsctl',
