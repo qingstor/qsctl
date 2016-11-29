2cc5fe3c912dc37356d332b103c0f132e1058c63
Jingwen Peng <pengsrc@yunify.com>
2016-11-29 13:20:20 +0800

    Update package information
    Signed-off-by: Jingwen Peng <pengsrc@yunify.com>

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'qsctl',
-    version = '1.0.4',
+    version = '1.0.5',
     description = 'Advanced command line tool for QingStor.',
     long_description = open('README.rst', 'rb').read().decode('utf-8'),
     keywords = 'qingcloud qingstor qsctl',
     author = 'Daniel Zheng',
     author_email = 'daniel@yunify.com',
-    url = 'https://docs.qingcloud.com',
+    url = 'https://www.qingstor.com',
     scripts = ['bin/qsctl', 'bin/qsctl.cmd'],
     packages = find_packages('.'),
-    package_dir = {'qsctl': 'qingstor',},
-    namespace_packages = ['qingstor',],
+    package_dir = {'qsctl': 'qingstor'},
+    namespace_packages = ['qingstor'],
     include_package_data = True,
     install_requires = [
         'argparse >= 1.1',
