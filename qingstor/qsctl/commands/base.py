dc419501c92ad9e88eeb7dbd733f80e80607c9f8
Xuanwo <xuanwo@yunify.com>
2017-01-09 16:41:56 +0800

    Refactor BaseCommand class
    Added:
    add get_client to use new sdk
    add get_buckets to get bucket's zone automatically
    
    Changed:
    replace qingcloud-sdk with qingstor-sdk
    format with google code style
    
    Removed:
    remove dependent on ENDPOINT constants
    remove get_connection function
    
    BREAKING CHANGE:
    default config path changed to ~/.qingstor/config.yaml
    config should use new version
    
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- qingstor/qsctl/commands/base.py
+++ qingstor/qsctl/commands/base.py
@@ -18,25 +18,25 @@ import os
 import sys
 import argparse
 
-from qingcloud.qingstor.connection import QSConnection
+from qingstor.sdk.config import Config
+from qingstor.sdk.service.qingstor import QingStor
 
-from ..constants import ENDPOINT, HTTP_OK, HTTP_OK_NO_CONTENT
+from ..constants import HTTP_OK, HTTP_OK_NO_CONTENT
 from ..utils import (
     load_conf,
     uni_print,
-    json_loads,
     to_unix_path,
     is_pattern_match,
-    validate_bucket_name,
-)
+    validate_bucket_name,)
+
 
 class BaseCommand(object):
-
     command = ""
     usage = ""
     description = ""
 
-    conn = None
+    client = None
+    bucket_map = {}
 
     @classmethod
     def add_common_arguments(cls, parser):
@@ -46,9 +46,8 @@ class BaseCommand(object):
             dest="config",
             action="store",
             type=str,
-            default="~/.qingcloud/config.yaml",
-            help="Configuration file"
-        )
+            default="~/.qingstor/config.yaml",
+            help="Configuration file")
 
     @classmethod
     def add_extra_arguments(cls, parser):
@@ -63,25 +62,25 @@ class BaseCommand(object):
         parser = argparse.ArgumentParser(
             prog='qsctl %s' % cls.command,
             usage=cls.usage,
-            description=cls.description
-        )
+            description=cls.description)
         cls.add_common_arguments(parser)
         cls.add_extra_arguments(parser)
         cls.add_transfer_arguments(parser)
         return parser
 
     @classmethod
-    def get_connection(cls, conf, options):
-        if cls.command in ("mb"):
-            host = ENDPOINT
-        else:
-            host = "%s.%s" % (conf["zone"], ENDPOINT)
+    def get_client(cls, conf):
+        config = Config().load_config_from_data(conf)
+        return QingStor(config)
 
-        return QSConnection(
-            qy_access_key_id=conf["qy_access_key_id"],
-            qy_secret_access_key=conf["qy_secret_access_key"],
-            host=host
-        )
+    @classmethod
+    def get_buckets(cls):
+        resp = cls.client.list_buckets()
+        if resp.status_code != HTTP_OK:
+            print("Error: Please check your configuration and you have "
+                  "enough permission to access qingstor service.")
+            sys.exit()
+        return resp["buckets"]
 
     @classmethod
     def send_request(cls, options):
@@ -89,28 +88,38 @@ class BaseCommand(object):
 
     @classmethod
     def validate_bucket(cls, bucket):
-        resp = cls.conn.make_request("HEAD", bucket)
-        status = resp.status
-        resp.close()
-        if status != HTTP_OK:
-            print("Error: Please check if bucket <%s> exists and you have " \
-                "enough permission to access it." % bucket)
-            sys.exit(-1)
+        if not cls.bucket_map.get(bucket):
+            cls.bucket_map[bucket] = ""
+            buckets = cls.get_buckets()
+            for i in buckets:
+                if bucket == i["name"]:
+                    cls.bucket_map[bucket] = i["location"]
+                    break
+            if cls.bucket_map[bucket] == "":
+                print("Error: Please check if bucket <%s> exists" % bucket)
+                sys.exit(-1)
+            current_bucket = cls.client.Bucket(bucket, cls.bucket_map[bucket])
+            resp = current_bucket.head()
+            if resp.status_code != HTTP_OK:
+                print("Error: Please check if you have "
+                      "enough permission to access it." % bucket)
+                sys.exit(-1)
 
     @classmethod
     def validate_local_path(cls, path):
         dirname = os.path.dirname(path)
         if dirname != "":
             if os.path.isfile(dirname):
-                print("Error: File with the same name '%s' already exists" \
-                    % dirname)
+                print("Error: File with the same name '%s' already exists" %
+                      dirname)
                 sys.exit(-1)
             elif not os.path.isdir(dirname):
                 try:
                     os.makedirs(dirname)
                     print("Directory '%s' created" % dirname)
                 except OSError as e:
-                    print("Error: Failed to create directory '%s': %s" % (dirname, e))
+                    print("Error: Failed to create directory '%s': %s" %
+                          (dirname, e))
                     sys.exit(-1)
 
     @classmethod
@@ -132,20 +141,21 @@ class BaseCommand(object):
 
     @classmethod
     def key_exists(cls, bucket, key):
-        resp = cls.conn.make_request("HEAD", bucket, key)
-        status = resp.status
-        resp.close()
-        return status == HTTP_OK
+        cls.validate_bucket(bucket)
+        current_bucket = cls.client.Bucket(bucket, cls.bucket_map[bucket])
+        resp = current_bucket.head_object(key)
+        return resp.status_code == HTTP_OK
 
     @classmethod
     def remove_key(cls, bucket, key):
-        resp = cls.conn.make_request("DELETE", bucket, key)
-        if resp.status != HTTP_OK_NO_CONTENT:
-            print(resp.read())
+        cls.validate_bucket(bucket)
+        current_bucket = cls.client.Bucket(bucket, cls.bucket_map[bucket])
+        resp = current_bucket.delete_object(key)
+        if resp.status_code != HTTP_OK_NO_CONTENT:
+            print(resp.content)
         else:
             statement = "Key <%s> deleted" % key
             uni_print(statement)
-        resp.close()
 
     @classmethod
     def confirm_key_remove(cls, key_name, options):
@@ -159,30 +169,31 @@ class BaseCommand(object):
         cls.validate_bucket(bucket)
         marker = ""
         while True:
-            keys, marker, _ = cls.list_multiple_keys(bucket, marker, prefix)
+            keys, marker, _ = cls.list_multiple_keys(
+                bucket, marker=marker, prefix=prefix)
             for item in keys:
-                key = item["key"] if sys.version > "3" else item["key"].encode('utf8')
+                key = item["key"] if sys.version > "3" else item["key"].encode(
+                    'utf8')
                 if cls.confirm_key_remove(key[len(prefix):], options):
                     cls.remove_key(bucket, key)
             if marker == "":
                 break
 
     @classmethod
-    def list_multiple_keys(cls, bucket, marker, prefix="", params=None):
+    def list_multiple_keys(cls,
+                           bucket,
+                           prefix="",
+                           delimiter="",
+                           marker="",
+                           limit=200):
         cls.validate_bucket(bucket)
-        if params == None:
-            params = {}
-        if prefix != "":
-            params["prefix"] = prefix
-        if marker != "" and cls.key_exists(bucket, marker):
-            params["marker"] = marker
-        resp = cls.conn.make_request("GET", bucket, params=params)
-        body = json_loads(resp.read())
-        keys = body["keys"]
-        dirs = body["common_prefixes"]
-        marker = body["marker"] if sys.version > "3" else body["marker"].encode('utf8')
-        resp.close()
-        return keys, marker, dirs
+        current_bucket = cls.client.Bucket(bucket, cls.bucket_map[bucket])
+        resp = current_bucket.list_objects(
+            marker=marker, prefix=prefix, delimiter=delimiter, limit=limit)
+        keys = resp["keys"]
+        dirs = resp["common_prefixes"]
+        next_marker = resp["next_marker"]
+        return keys, next_marker, dirs
 
     @classmethod
     def main(cls, args):
@@ -192,11 +203,12 @@ class BaseCommand(object):
 
         # Load config file
         conf = load_conf(options.config)
+
         if conf is None:
             sys.exit(-1)
 
-        # Get connection to the server
-        cls.conn = cls.get_connection(conf, options)
+        # Get client of qingstor
+        cls.client = cls.get_client(conf)
 
         # Send request
         return cls.send_request(options)
