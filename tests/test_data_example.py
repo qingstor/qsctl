69626430a1733a25eb4106dde5e25bcd40fedd98
Xuanwo <xuanwo@yunify.com>
2017-01-10 17:05:18 +0800

    tests: Update test_data_example to new schema
    fixed https://github.com/yunify/qsctl/issues/8
    
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- tests/test_data_example.py
+++ tests/test_data_example.py
@@ -1,6 +1,6 @@
-zone = ''
-valid_bucket = ''
-invalid_bucket = ''
+zone = 'pek3a'
+test_bucket1 = '<your-test-bucket1>'
+test_bucket2 = '<your-test-bucket2>'
 
 retry_wait_time = 3
 max_retries = 60
