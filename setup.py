64aa2faaae92f86722817c9395a0a2f5a6e3a631
Xuanwo <xuanwo@yunify.com>
2017-02-27 22:23:11 +0800

    Bump to version 1.3.0
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qsctl',
-    version='1.2.3',
+    version='1.3.0',
     description='Advanced command line tool for QingStor.',
     long_description=open('README.rst', 'rb').read().decode('utf-8'),
     keywords='yunify qingcloud qingstor qsctl object_storage',
@@ -19,7 +19,7 @@ setup(
     install_requires=[
         'argparse >= 1.1',
         'PyYAML >= 3.1',
-        'qingstor-sdk >= 2.0.2',
+        'qingstor-sdk >= 2.1.0',
         'docutils >= 0.10',
         'tqdm >= 4.0.0'
     ])
