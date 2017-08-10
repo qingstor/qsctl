99bf415b962a4d1437dc93db9d675cfe24741d23
Xuanwo <xuanwo@yunify.com>
2017-08-10 22:47:21 +0800

    Update dependence for python2
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- setup.py
+++ setup.py
@@ -5,15 +5,23 @@ from setuptools import setup, find_packages
 from qingstor.qsctl import __version__
 
 install_requires = [
-    'argparse >= 1.1',
     'PyYAML >= 3.1',
-    'qingstor-sdk >= 2.1.0',
+    'qingstor-sdk >= 2.1.3',
     'docutils >= 0.10',
     'tqdm >= 4.0.0'
 ]
 
+# python version under 2.7.9 should use requests[security]
 if version_info[:3] < (2, 7, 9):
     install_requires.append("requests[security]")
+
+# python version under 2.7 do not have argparse
+if version_info[:3] < (2, 7):
+    install_requires.append("argparse >= 1.1")
+
+# python version under 3 do not have concurrent.futures
+if version_info[:3] < (3, 2):
+    install_requires.append("futures")
 
 setup(
     name='qsctl',
