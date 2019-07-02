8731e8a11f61a9c663db5d4cc535dda9d2f1263c
Xuanwo <xuanwo@yunify.com>
2019-07-02 16:05:39 +0800

    Add copy logic poc

--- cmd/cp.go
+++ cmd/cp.go
@@ -1,1 +1,28 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/yunify/qsctl/action"
+)
+
+// CpCommand will handle copy command.
+var CpCommand = &cobra.Command{
+	Use:   "cp <source-path> <dest-path>",
+	Short: "copy from/to qingstor",
+	Long:  "qsctl cp can copy file/folder/stdin to qingstor or copy qingstor objects to local/stdout",
+	Example: `  Copy file:       qsctl cp /path/to/file qs://prefix/a
+  Copy folder:     qsctl cp qs://prefix/a /path/to/folder -r
+  Read from stdin: cat /path/to/file | qsctl cp - qs://prefix/stdin
+  Write to stdout: qsctl cp qs://prefix/b - > /path/to/file
+`,
+	Args: cobra.ExactArgs(2),
+	RunE: run,
+}
+
+func run(cmd *cobra.Command, args []string) (err error) {
+	err = action.Copy(args[0], args[1])
+	if err != nil {
+		panic(err)
+	}
+	return
+}
--- helper/qingstor.go
+++ helper/qingstor.go
@@ -2,35 +2,26 @@ package helper
 
 import (
 	"github.com/yunify/qingstor-sdk-go/v3/service"
-
 	"github.com/yunify/qsctl/constants"
 	"github.com/yunify/qsctl/contexts"
 )
 
 // ListObjects will list object by prefix.
 func ListObjects(prefix string) (
-	commonPrefix chan string, keys chan service.KeyType, err error,
+	keys chan service.KeyType, err error,
 ) {
-	bucket, _ := contexts.Service.Bucket("test", "test")
-
-	commonPrefix = make(chan string)
 	keys = make(chan service.KeyType)
 	defer close(keys)
-	defer close(commonPrefix)
 
 	marker := ""
 	for {
-		resp, err := bucket.ListObjects(&service.ListObjectsInput{
+		resp, err := contexts.Bucket.ListObjects(&service.ListObjectsInput{
 			Limit:  service.Int(200),
 			Marker: service.String(marker),
 			Prefix: service.String(prefix),
 		})
 		if err != nil {
-			return nil, nil, err
-		}
-
-		for _, v := range resp.CommonPrefixes {
-			commonPrefix <- *v
+			return nil, err
 		}
 
 		for _, v := range resp.Keys {
