b025dc2a47929a4a5276a3fb3f503738c3dd92cb
Xuanwo <xuanwo@yunify.com>
2019-07-02 16:04:41 +0800

    constants: Add available types

--- constants/types.go
+++ constants/types.go
@@ -2,3 +2,10 @@ package constants
 
 // DirectoryContentType is the content type qingstor used for directory.
 const DirectoryContentType = "application/x-directory"
+
+const (
+	// MaximumObjectSize is the max object size for a single object, 50TB.
+	MaximumObjectSize = 50 * 1024 * 1024 * 1024 * 1024
+	// DefaultMultipartBoundarySize is the default boundary size for multipart, 1GB.
+	DefaultMultipartBoundarySize = 1024 * 1024 * 1024
+)
