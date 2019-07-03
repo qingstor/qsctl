29b190276b0a8c20857c5fcff940ca557137b900
Xuanwo <xuanwo@yunify.com>
2019-07-03 14:58:47 +0800

    action: Add poc of copy from stdin to remote path

--- action/copy.go
+++ action/copy.go
@@ -1,9 +1,15 @@
 package action
 
 import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
 
 	"github.com/yunify/qsctl/constants"
+	"github.com/yunify/qsctl/helper"
 )
 
 // Copy will handle all copy actions.
@@ -25,14 +31,26 @@ func Copy(src, dest string) (err error) {
 			panic(err)
 		}
 
-		switch r.(type) {
-		case io.ReadSeeker:
+		switch x := r.(type) {
+		case *os.File:
+			if x.Name() == "/dev/stdin" {
+				err = CopyNotSeekableFileToRemote(r, objectKey)
+				return err
+			}
 			err = CopySeekableFileToRemote(r, objectKey)
+			return err
+		case io.ReadSeeker:
+			fmt.Printf("Start CopySeekableFileToRemote")
+			err = CopySeekableFileToRemote(r, objectKey)
+			if err != nil {
+				panic(err)
+			}
 		default:
+			fmt.Printf("Start CopyNotSeekableFileToRemote")
 			err = CopyNotSeekableFileToRemote(r, objectKey)
-		}
-		if err != nil {
-			panic(err)
+			if err != nil {
+				panic(err)
+			}
 		}
 		return nil
 
@@ -51,5 +69,39 @@ func CopySeekableFileToRemote(r io.Reader, objectKey string) (err error) {
 
 // CopyNotSeekableFileToRemote will copy a not seekable file to remote.
 func CopyNotSeekableFileToRemote(r io.Reader, objectKey string) (err error) {
+	uploadID, err := helper.InitiateMultipartUpload(objectKey)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Upload ID is %s.\n", uploadID)
+
+	partNumber := 0
+
+	for {
+		lr := io.LimitReader(r, constants.DefaultPartSize)
+		b, err := ioutil.ReadAll(lr)
+		l := len(b)
+		if l == 0 {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Read %d bytes.\n", l)
+		err = helper.UploadMultipart(objectKey, uploadID, int64(l), partNumber, md5.Sum(b), bytes.NewReader(b))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Part %d uploaded.\n", partNumber)
+
+		partNumber++
+	}
+
+	err = helper.CompleteMultipartUpload(objectKey, uploadID, partNumber)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Upload ID %s for %s finished.\n", uploadID, objectKey)
 	return nil
 }
--- constants/types.go
+++ constants/types.go
@@ -4,8 +4,14 @@ package constants
 const DirectoryContentType = "application/x-directory"
 
 const (
+	// MaximumMultipartNumber is the max multipart number.
+	MaximumMultipartNumber = 10000
+	// MaximumPartSize is the max part size for single part, 5GB.
+	MaximumPartSize = 5 * 1024 * 1024 * 1024
 	// MaximumObjectSize is the max object size for a single object, 50TB.
-	MaximumObjectSize = 50 * 1024 * 1024 * 1024 * 1024
-	// DefaultMultipartBoundarySize is the default boundary size for multipart, 1GB.
-	DefaultMultipartBoundarySize = 1024 * 1024 * 1024
+	MaximumObjectSize = MaximumMultipartNumber * MaximumPartSize
+	// MaximumAutoMultipartSize is the size limit for auto part size detect.
+	MaximumAutoMultipartSize = MaximumPartSize / 5
+	// DefaultPartSize is the default part size, 128MB.
+	DefaultPartSize = 128 * 1024 * 1024
 )
