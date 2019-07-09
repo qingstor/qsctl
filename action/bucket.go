fa9b0afe183ca017ab584ad2905a2eedd80f59fb
Prnyself <281239768@qq.com>
2019-07-09 17:10:59 +0800

    action/bucket: Move put bucket logic by qingstor into helpers

--- action/bucket.go
+++ action/bucket.go
@@ -4,6 +4,7 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"github.com/yunify/qsctl/contexts"
+	"github.com/yunify/qsctl/helper"
 )
 
 // MakeBucket will make a bucket with specific name.
@@ -11,21 +12,10 @@ func MakeBucket(bucketPath string) (err error) {
 	// Get bucket name from path
 	bucketName, _, err := ParseQsPath(bucketPath)
 	if err != nil {
-		log.Errorf("Parse qs path <%s> failed [%v]",
-			bucketPath, err)
 		return
 	}
 	// Init bucket
-	bucket, err := contexts.Service.Bucket(bucketName, contexts.Zone)
-	if err != nil {
-		log.Errorf("Initial bucket <%s> in zone <%s> failed [%v]",
-			bucketName, contexts.Zone, err)
-		return
-	}
-	// Request and create bucket
-	if _, err = bucket.Put(); err != nil {
-		log.Errorf("Make bucket <%s> in zone <%s> failed [%v]",
-			bucketName, contexts.Zone, err)
+	if err = helper.PutBucket(bucketName, contexts.Zone); err != nil {
 		return
 	}
 	log.Infof("Bucket <%s> created.", bucketName)
