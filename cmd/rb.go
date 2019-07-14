6be36760bb86ba706937f089d8eb2dd06203ad9b
Lance <Prnyself@users.noreply.github.com>
2019-07-14 16:47:50 +0800

    cmd: Add rb command to remove an empty bucket (#91)
    Co-authored-by: Xuanwo <xuanwo@yunify.com>

--- cmd/rb.go
+++ cmd/rb.go
@@ -1,1 +1,24 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/yunify/qsctl/v2/action"
+	"github.com/yunify/qsctl/v2/utils"
+)
+
+// RbCommand will handle remove object command.
+var RbCommand = &cobra.Command{
+	Use:   "rb [qs://]<bucket_name>",
+	Short: "delete a bucket",
+	Long:  "qsctl rb delete an empty qingstor bucket",
+	Example: utils.AlignPrintWithColon(
+		"delete an empty bucket: qsctl rb qs://bucket-name",
+	),
+	Args: cobra.ExactArgs(1),
+	RunE: rbRun,
+}
+
+func rbRun(_ *cobra.Command, args []string) (err error) {
+	return action.RemoveBucket(args[0])
+}
