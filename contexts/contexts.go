abc83453d30c01ee81035f160fad3627befae39f
Lance <281239768@qq.com>
2019-08-13 18:14:04 +0800

    contexts: Remove contexts.context (#104)

--- contexts/contexts.go
+++ contexts/contexts.go
@@ -1,8 +1,6 @@
 package contexts
 
 import (
-	"context"
-
 	"github.com/yunify/qsctl/v2/storage"
 )
 
@@ -19,19 +17,4 @@ func SetupServices() (err error) {
 	}
 
 	return
-}
-
-// SetContext set k-v into specific ctx
-func SetContext(ctx context.Context, k, v interface{}) context.Context {
-	return context.WithValue(ctx, k, v)
-}
-
-// FromContext get value from context with the specific k
-func FromContext(ctx context.Context, k interface{}) interface{} {
-	return ctx.Value(k)
-}
-
-// NewCmdCtx returns a new CmdCtx with empty background
-func NewCmdCtx() context.Context {
-	return context.Background()
 }
