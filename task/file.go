ad8d5ed2515bb7bd3f61ab3ce168152098912532
Xuanwo <xuanwo@yunify.com>
2019-08-15 16:44:14 +0800

    Refactor actions (#105)
    * Simplify Makefile
    
    * contexts: Add global task pool
    
    * Add new task execute logic
    
    * Refactor to meet task execution changes
    
    * misc: Fix coverage dir not created
    
    * Address a bit review

---
+++ task/file.go
@@ -0,0 +1,57 @@
+package task
+
+// CopyLargeFileTask will execute CopyPartialFile Task
+type CopyLargeFileTask struct {
+	Todo
+
+	FilePath
+	ObjectKey
+	UploadID
+	TotalParts
+	WaitGroup
+}
+
+// Run implement navvy.Task
+func (t *CopyLargeFileTask) Run() {
+	SubmitNextTask(t)
+}
+
+// CopyPartialFileTask will execute CopyPartialFile Task
+type CopyPartialFileTask struct {
+	Todo
+
+	MD5Sum
+	ContentLength
+	PartNumber
+	UploadID
+	FilePath
+	ObjectKey
+	Offset
+	WaitGroup
+}
+
+// Run implement navvy.Task
+func (t *CopyPartialFileTask) Run() {
+	SubmitNextTask(t)
+}
+
+// NewCopyPartialFileTask will create a new Task.
+func NewCopyPartialFileTask(
+	objectKey, filePath, uploadID string,
+	partNumber int,
+	offset, contentLength int64,
+) *CopyPartialFileTask {
+	t := &CopyPartialFileTask{}
+	t.SetPartNumber(partNumber)
+	t.SetOffset(offset)
+	t.SetContentLength(contentLength)
+	t.SetUploadID(uploadID)
+	t.SetFilePath(filePath)
+	t.SetObjectKey(objectKey)
+
+	t.AddTODOs(
+		NewSeekableMD5SumTask,
+		NewMultipartObjectUploadTask,
+	)
+	return t
+}
--- task/utils_test.go
+++ task/utils_test.go
@@ -1,4 +1,4 @@
-package action
+package task
 
 import (
 	"io/ioutil"
