562d6d647c264824192bff089877a7fab673ab14
Lance <281239768@qq.com>
2019-09-24 16:27:28 +0800

    cmd/mb: Implement make bucket (#110)

--- cmd/qsctl/mb.go
+++ cmd/qsctl/mb.go
@@ -4,8 +4,15 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/yunify/qsctl/v2/constants"
+	"github.com/yunify/qsctl/v2/storage"
+	"github.com/yunify/qsctl/v2/task"
+	taskUtils "github.com/yunify/qsctl/v2/task/utils"
 	"github.com/yunify/qsctl/v2/utils"
 )
+
+var mbInput struct {
+	Zone string
+}
 
 // MbCommand will handle make bucket command.
 var MbCommand = &cobra.Command{
@@ -27,19 +34,53 @@ bucket name should follow DNS name rule with:
 	PreRunE: validateMbFlag,
 }
 
-func mbRun(_ *cobra.Command, args []string) (err error) {
-	// Package handler
+func mbParse(t *task.MakeBucketTask, args []string) (err error) {
+	// Parse flags.
+	t.SetZone(mbInput.Zone)
 	return nil
 }
 
+func mbRun(_ *cobra.Command, args []string) (err error) {
+	t := task.NewMakeBucketTask(func(t *task.MakeBucketTask) {
+		if err = mbParse(t, args); err != nil {
+			return
+		}
+		keyType, bucketName, _, e := taskUtils.ParseKey(args[0])
+		if e != nil {
+			err = e
+			return
+		}
+		if keyType != constants.KeyTypeBucket {
+			err = constants.ErrorQsPathInvalid
+			return
+		}
+		t.SetBucketName(bucketName)
+
+		stor, e := storage.NewQingStorObjectStorage()
+		if e != nil {
+			err = e
+			return
+		}
+		t.SetStorage(stor)
+
+		if err = stor.SetupBucket(t.GetBucketName(), t.GetZone()); err != nil {
+			return
+		}
+	})
+
+	t.Run()
+	t.Wait()
+	return
+}
+
 func initMbFlag() {
-	MbCommand.Flags().StringVarP(&zone, constants.ZoneFlag, "z",
+	MbCommand.Flags().StringVarP(&mbInput.Zone, constants.ZoneFlag, "z",
 		"", "in which zone to make the bucket (required)")
 }
 
 func validateMbFlag(_ *cobra.Command, _ []string) error {
 	// check zone flag (required)
-	if zone == "" {
+	if mbInput.Zone == "" {
 		return constants.ErrorZoneRequired
 	}
 	return nil
