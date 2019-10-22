0959918c28b6170d0b91a701f6b6bebcd7b95da7
Xuanwo <xuanwo@yunify.com>
2019-10-22 17:38:44 +0800

    task: Add tests for generated task

--- utils/test.go
+++ utils/test.go
@@ -28,6 +28,28 @@ type EmptyTask struct {
 func (t *EmptyTask) Run() {
 }
 
+// NewCallbackTask will create a new callback test.
+func NewCallbackTask(fn func()) *CallbackTask {
+	return &CallbackTask{
+		fn: fn,
+	}
+}
+
+// CallbackTask is the callback task.
+type CallbackTask struct {
+	types.ID
+	types.Fault
+	types.Pool
+	types.Todo
+
+	fn func()
+}
+
+// Run implement navvy.Task interface.
+func (t *CallbackTask) Run() {
+	t.fn()
+}
+
 // GenerateTestStream will generate a test stream.
 func GenerateTestStream() (buf *bytes.Buffer, size int64, md5sum []byte) {
 	buf = bytes.NewBuffer(nil)
