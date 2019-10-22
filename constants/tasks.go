0fae3d969523d687dd9ea6154ac58987f5f18eee
Xuanwo <xuanwo@yunify.com>
2019-10-22 15:36:39 +0800

    Code cleanup

--- constants/tasks.go
+++ constants/tasks.go
@@ -11,28 +11,6 @@ const (
 	FlowToRemote
 )
 
-// PathType is the type for path
-type PathType uint8
-
-// All available path type
-const (
-	PathTypeInvalid PathType = iota
-	PathTypeFile
-	PathTypeStream
-	PathTypeLocalDir
-)
-
-// KeyType is the type for key
-type KeyType uint8
-
-// All available key type
-const (
-	KeyTypeInvalid KeyType = iota
-	KeyTypeBucket
-	KeyTypeObject
-	KeyTypePseudoDir
-)
-
 // ListType is the type for list
 type ListType uint8
 
