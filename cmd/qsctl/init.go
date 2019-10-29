e6b9bbd391f2b4c75895c69f810a7f67abefdc0b
Prnyself <281239768@qq.com>
2019-10-29 15:35:04 +0800

    cmd/utils: Use survey instead

--- cmd/qsctl/init.go
+++ cmd/qsctl/init.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"gopkg.in/yaml.v2"
 
+	"github.com/yunify/qsctl/v2/cmd/utils"
 	"github.com/yunify/qsctl/v2/constants"
 )
 
@@ -84,18 +81,21 @@ func initConfig() (err error) {
 		viper.SetConfigName("config")
 	}
 
+	// try to read config from path set above
 	err = viper.ReadInConfig()
 	if err != nil {
 		switch err.(type) {
 		case viper.ConfigFileNotFoundError:
+			// if config file not found, try to get access key & secret key from env
 			log.Warnf("Config not loaded, use default and environment value instead.")
+			// if env not set, start interactive setup
 			if viper.GetString(constants.ConfigAccessKeyID) == "" && viper.GetString(constants.ConfigSecretAccessKey) == "" {
-				log.Infof("AK and SK not found. Please setup your config now.")
-				fileName, err := SetupConfigInteractive()
+				log.Infof("AccessKey and SecretKey not found. Please setup your config now, or exit and setup manually.")
+				fileName, err := utils.SetupConfigInteractive()
 				if err != nil {
-					return err
+					return fmt.Errorf("setup config failed [%v], please try again", err)
 				}
-				log.Infof("Your config has been set to <%v>.", fileName)
+				log.Infof("Your config has been set to <%v>. You can still modify it manually.", fileName)
 				viper.SetConfigFile(fileName)
 				if err = viper.ReadInConfig(); err != nil {
 					return err
@@ -129,115 +129,4 @@ func initGlobalFlag() {
 		false, "enable benchmark or not")
 	// Overwrite the default help flag to free -h shorthand.
 	rootCmd.PersistentFlags().Bool("help", false, "help for this command")
-}
-
-type inputConfig struct {
-	AccessKeyID     string `yaml:"access_key_id"`
-	SecretAccessKey string `yaml:"secret_access_key"`
-
-	Host     string `yaml:"host"`
-	Port     string `yaml:"port"`
-	Protocol string `yaml:"protocol"`
-	LogLevel string `yaml:"log_level"`
-}
-
-// SetupConfigInteractive setup input config interactively
-func SetupConfigInteractive() (fileName string, err error) {
-	in := newInputConfig()
-
-	// multiple times to set config if not confirmed
-	for {
-		fmt.Printf("Access Key ID: ")
-		if _, err = fmt.Scanf("%s", &in.AccessKeyID); err != nil {
-			return "", err
-		}
-
-		fmt.Printf("Secret Access Key: ")
-		if _, err = fmt.Scanf("%s", &in.SecretAccessKey); err != nil {
-			return "", err
-		}
-
-		if !confirm("Do you apply qsctl for QingStor public cloud") {
-			fmt.Printf("Host: ")
-			if _, err = fmt.Scanf("%s", &in.Host); err != nil {
-				return "", err
-			}
-
-			fmt.Printf("Protocol [http/https]: ")
-			if _, err = fmt.Scanf("%s", &in.Protocol); err != nil {
-				return "", err
-			}
-
-			fmt.Printf("Port: ")
-			if _, err = fmt.Scanf("%s", &in.Port); err != nil {
-				return "", err
-			}
-		}
-
-		fmt.Printf("Log level [low to high: debug/info/warn/error/fatal]: ")
-		if _, err = fmt.Scanf("%s", &in.LogLevel); err != nil {
-			return "", err
-		}
-
-		b, err := yaml.Marshal(in)
-		if err != nil {
-			return "", err
-		}
-
-		fmt.Println("Your config listed below:")
-		fmt.Printf("\n%v\n", string(b))
-
-		if confirm("Confirm") {
-			break
-		}
-		// reset the input
-		in = newInputConfig()
-	}
-
-	b, err := yaml.Marshal(in)
-	if err != nil {
-		return "", err
-	}
-
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
-
-	fileName = filepath.Join(homeDir, ".qingstor/config.yaml")
-	f, err := os.Create(fileName)
-	if err != nil {
-		return "", err
-	}
-	defer func() {
-		_ = f.Close()
-	}()
-
-	if _, err = f.Write(b); err != nil {
-		return "", err
-	}
-	return fileName, nil
-}
-
-// newInputConfig setup inputConfig and return the struct
-func newInputConfig() inputConfig {
-	return inputConfig{
-		AccessKeyID:     "",
-		SecretAccessKey: "",
-		Host:            constants.DefaultHost,
-		Port:            constants.DefaultPort,
-		Protocol:        constants.DefaultProtocol,
-		LogLevel:        constants.DefaultLogLevel,
-	}
-}
-
-func confirm(tip string) bool {
-	var out string
-	fmt.Printf("%s? [yes/no] ", tip)
-
-	if _, err := fmt.Scanf("%s", &out); err != nil {
-		panic(err)
-	}
-
-	return strings.ToLower(out[:1]) == "y"
 }
