0b0ca91becc76c50fbca3a15579b48a30ee6fc92
Xuanwo <xuanwo@yunify.com>
2019-10-31 14:29:20 +0800

    task: Remove || operation in go generate

--- task/doc.go
+++ task/doc.go
@@ -1,6 +1,5 @@
 package task
 
-//go:generate rm *generated*.go -f || true
 //go:generate go run ../internal/cmd/generator/tasks
 //go:generate mockgen -package mock -destination ../pkg/mock/storager.go github.com/Xuanwo/storage Storager,Servicer
 //go:generate mockgen -package mock -destination ../pkg/mock/io.go io ReadCloser
