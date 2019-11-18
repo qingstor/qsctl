4e939db8ec5e51eb0d86fcca4194342853a7d5cc
Xuanwo <xuanwo@yunify.com>
2019-11-18 12:11:40 +0800

    task: Add unit tests for segment

---
+++ task/segment_test.go
@@ -0,0 +1,68 @@
+package task
+
+import (
+	"errors"
+	"testing"
+
+	typ "github.com/Xuanwo/storage/types"
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/yunify/qsctl/v2/pkg/fault"
+	"github.com/yunify/qsctl/v2/pkg/mock"
+)
+
+func TestSegmentInitTask_run(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("normal", func(t *testing.T) {
+		store := mock.NewMockStorager(ctrl)
+		path := uuid.New().String()
+		segmentID := uuid.New().String()
+
+		task := SegmentInitTask{}
+		task.SetStorage(store)
+		task.SetPath(path)
+		task.SetPartSize(1000)
+
+		store.EXPECT().InitSegment(gomock.Any(), gomock.Any()).DoAndReturn(
+			func(inputPath string, pairs ...*typ.Pair) (string, error) {
+				assert.Equal(t, inputPath, path)
+				assert.Equal(t, pairs[0].Value.(int64), int64(1000))
+				return segmentID, nil
+			},
+		)
+
+		task.run()
+
+		assert.Equal(t, segmentID, task.GetSegmentID())
+	})
+
+	t.Run("init segment returned error", func(t *testing.T) {
+		store := mock.NewMockStorager(ctrl)
+		path := uuid.New().String()
+
+		task := SegmentInitTask{}
+		task.SetFault(fault.New())
+		task.SetStorage(store)
+		task.SetPath(path)
+		task.SetPartSize(1000)
+
+		store.EXPECT().String().DoAndReturn(func() string {
+			return "test storager"
+		})
+		store.EXPECT().InitSegment(gomock.Any(), gomock.Any()).DoAndReturn(
+			func(inputPath string, pairs ...*typ.Pair) (string, error) {
+				assert.Equal(t, inputPath, path)
+				assert.Equal(t, pairs[0].Value.(int64), int64(1000))
+				return "", errors.New("test")
+			},
+		)
+
+		task.run()
+
+		assert.False(t, task.ValidateSegmentID())
+		assert.True(t, task.GetFault().HasError())
+	})
+}
