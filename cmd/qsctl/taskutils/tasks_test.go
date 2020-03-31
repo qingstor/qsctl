3472a78a75872f5f6cd9b5645b0dfe692990f4fd
Prnyself <281239768@qq.com>
2020-03-31 10:41:56 +0800

    cmd/taskutils: Fix bug that makes travis out of memory.

--- cmd/qsctl/taskutils/tasks_test.go
+++ cmd/qsctl/taskutils/tasks_test.go
@@ -8,50 +8,82 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// random an int more than 0 and less than 20 to restrict the memory use.
 func randIntP() int {
 	r := rand.New(rand.NewSource(time.Now().Unix()))
-	for {
-		tmp := r.Int31()
-		if tmp > 0 {
-			return int(tmp)
-		}
-	}
+	return int(r.Int31()%100 + 1)
 }
 
 func TestNewAtServiceTask(t *testing.T) {
 	tests := []struct {
-		name string
-		size int
+		name      string
+		size      int
+		wantPanic bool
 	}{
 		{
-			name: "normal",
-			size: randIntP(),
+			name:      "normal",
+			size:      randIntP(),
+			wantPanic: false,
+		},
+		{
+			name:      "zero size",
+			size:      0,
+			wantPanic: true,
+		},
+		{
+			name:      "negative size",
+			size:      -randIntP(),
+			wantPanic: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := NewAtServiceTask(tt.size)
-			assert.Equal(t, tt.size, got.GetPool().Cap(), tt.name)
-			assert.False(t, got.GetFault().HasError(), tt.name)
+			if tt.wantPanic {
+				assert.Panics(t, func() {
+					NewAtServiceTask(tt.size)
+				}, tt.name)
+			} else {
+				got := NewAtServiceTask(tt.size)
+				assert.Equal(t, tt.size, got.GetPool().Cap(), tt.name)
+				assert.False(t, got.GetFault().HasError(), tt.name)
+			}
 		})
 	}
 }
 
 func TestNewAtStorageTask(t *testing.T) {
 	tests := []struct {
-		name string
-		size int
+		name      string
+		size      int
+		wantPanic bool
 	}{
 		{
-			name: "normal",
-			size: randIntP(),
+			name:      "normal",
+			size:      randIntP(),
+			wantPanic: false,
+		},
+		{
+			name:      "zero size",
+			size:      0,
+			wantPanic: true,
+		},
+		{
+			name:      "negative size",
+			size:      -randIntP(),
+			wantPanic: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := NewAtStorageTask(tt.size)
-			assert.Equal(t, tt.size, got.GetPool().Cap(), tt.name)
-			assert.False(t, got.GetFault().HasError(), tt.name)
+			if tt.wantPanic {
+				assert.Panics(t, func() {
+					NewAtStorageTask(tt.size)
+				}, tt.name)
+			} else {
+				got := NewAtStorageTask(tt.size)
+				assert.Equal(t, tt.size, got.GetPool().Cap(), tt.name)
+				assert.False(t, got.GetFault().HasError(), tt.name)
+			}
 		})
 	}
 }
@@ -63,15 +95,32 @@ func TestNewBetweenStorageTask(t *testing.T) {
 		wantPanic bool
 	}{
 		{
-			name: "normal",
-			size: randIntP(),
+			name:      "normal",
+			size:      randIntP(),
+			wantPanic: false,
+		},
+		{
+			name:      "zero size",
+			size:      0,
+			wantPanic: true,
+		},
+		{
+			name:      "negative size",
+			size:      -randIntP(),
+			wantPanic: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := NewBetweenStorageTask(tt.size)
-			assert.Equal(t, tt.size, got.GetPool().Cap(), tt.name)
-			assert.False(t, got.GetFault().HasError(), tt.name)
+			if tt.wantPanic {
+				assert.Panics(t, func() {
+					NewBetweenStorageTask(tt.size)
+				}, tt.name)
+			} else {
+				got := NewBetweenStorageTask(tt.size)
+				assert.Equal(t, tt.size, got.GetPool().Cap(), tt.name)
+				assert.False(t, got.GetFault().HasError(), tt.name)
+			}
 		})
 	}
 }
